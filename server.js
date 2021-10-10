fe8ccabf664437a2e0540f4bb38c8cefd806e78e
Steve Bendrick <steven.bendrick@gmail.com>
2021-10-09 23:48:44 -0400

    notes now load on note page load

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const PORT = 3001;
 
 app.use(express.static("public"));
 
+const notes = require('./db/db.json')
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);

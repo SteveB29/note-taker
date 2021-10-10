c281a42c9d8c08bb47f630df2c5260b25dd98563
Steve Bendrick <steven.bendrick@gmail.com>
2021-10-10 00:43:35 -0400

    hitting save button sends note object to post route

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+
+// makes all static files available on page load
 app.use(express.static("public"));
 
 const notes = require('./db/db.json')
@@ -15,6 +20,11 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  res.json(notes);
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);

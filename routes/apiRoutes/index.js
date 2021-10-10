ef8c0f95d8b19056093adaae032866685256d6fd
Steve Bendrick <steven.bendrick@gmail.com>
2021-10-10 18:03:24 -0400

    added comments

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,16 +5,20 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 
-const writeToDB = (notes) => {
+// function to write current notes variable to db.json
+// Not added to own file as only one function and new file not necessary at this point in development
+function writeToDB(notes) {
   fs.writeFileSync(
     path.join(__dirname, '../../db/db.json'),
     JSON.stringify(notes, null, 2));
 }
 
+// get the current notes and sends back for notes section loading
 router.get('/notes', (req, res) => {
   res.json(notes);
 });
 
+// is called when new note is saved. Adds new note to the notes variable, updates db.json, and responds with current notes
 router.post('/notes', (req, res) => {
   const noteToAdd = req.body;
   noteToAdd.id = uuidv4();
@@ -23,6 +27,7 @@ router.post('/notes', (req, res) => {
   res.json(notes);
 });
 
+// is called when a note is deleted. Filters deleted note from the notes variable, updates db.json, and responds with current notes
 router.delete('/notes/:id', (req, res) => {
   const deleteNote = req.params.id;
   notes = notes.filter(note => note.id != deleteNote);
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
 
+// if page /notes is called, responds by sending notes.html
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// for all other page calls that are not defined, loads index.html
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
